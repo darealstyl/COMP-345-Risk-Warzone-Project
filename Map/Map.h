07afe05627112bf026daee36340bb9937c8568e7
Michael <osujim2012@gmail.com>
2022-03-05 11:59:54 -0500

    Migrate MapLoader to Map duo

--- Map/Map.h
+++ Map/Map.h
@@ -63,3 +63,13 @@ public:
 	~Map();
 
 };
+
+// <-- MAP LOADER -->
+class MapLoader {
+public:
+	static Map& createMap(string filename);
+	static Continent& createContinent(string&);
+	static Territory& createTerritory(string&, unordered_map<int, Continent*>, int&);
+	static void processBorders(string& line, unordered_map<int, Territory*> countries);
+	static vector<string>& splitInput(const string&, const char);
+};
--- Map/MapDriver.cpp
+++ Map/MapDriver.cpp
@@ -1,4 +1,4 @@
-#include "MapLoader.h"
+#include "Map.h"
 #include <iostream>
 using namespace std;
 

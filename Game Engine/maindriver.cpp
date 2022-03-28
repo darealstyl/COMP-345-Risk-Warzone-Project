1025b0a1d985efbaadf093064e600bc3992e0708
Michael <osujim2012@gmail.com>
2022-03-27 23:08:36 -0400

    Rename driver

--- Game Engine/maindriver.cpp
+++ Game Engine/maindriver.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int main() {
-	GameEngine forcewingame;;
-	forcewingame.FORCEWIN = true;
-	forcewingame.startupPhase();
+	GameEngine* forcewingame = new GameEngine();
+	forcewingame->FORCEWIN = true;
+	forcewingame->startupPhase();
 	cout << "Warning: next game will go by very fast and you won't be able to observe without breakpoints." << endl;
-	
+	delete forcewingame;
 	
 	GameEngine normalgame;
 	normalgame.startupPhase();
